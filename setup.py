2560ef889e8e3df735263db1ad7765281138b8f7
Toshiki Kataoka <kataoka@preferred.jp>
2023-07-24 16:36:55 +0900

    feat: add python3.11, drop python3.6 (#38)
    * fix remove py36
    
    * fix add py311
    
    * revert change to lint extra
    
    * remove lint extras from testenv
    
    * restore lint extras to latest, development
    
    ---------
    
    Co-authored-by: pigooosuke <hifirole@gmail.com>

--- setup.py
+++ setup.py
@@ -22,11 +22,11 @@ setup(
     url="https://github.com/pfnet/pysen",
     license="MIT License",
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
