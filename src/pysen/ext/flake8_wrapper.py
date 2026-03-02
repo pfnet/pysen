d57b2bb6e5f54cb5ca2ea6c84a3cae9fb4c93838
04cb <0x04cb@gmail.com>
2026-03-02 08:41:25 +0800

    Fix flake8 comments: replace placeholder text in E701 and E704 descriptions

--- src/pysen/ext/flake8_wrapper.py
+++ src/pysen/ext/flake8_wrapper.py
@@ -68,13 +68,13 @@ class Flake8Setting(SettingBase):
         if not _contains(new.ignore, "E701"):
             new.ignore.append("E701")
             new._comments.append(
-                "# (E701) black will collapse ... only functions etc. to a single line"
+                "# (E701) black will collapse multiple statements on one line (colon)"
             )
 
         if not _contains(new.ignore, "E704"):
             new.ignore.append("E704")
             new._comments.append(
-                "# (E704) black will collapse ... only functions etc. to a single line"
+                "# (E704) black will collapse multiple statements on one line (def)"
             )
 
         W503_or_504_enabled = _contains(new.ignore, "W503") or _contains(
